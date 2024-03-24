a5d54891af8a775835d381651c02194d7303e851
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-03-24 18:11:02 +0300

    feat: resolve getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -98,9 +98,13 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  return (
+    Math.abs(new Date(dateStart) - new Date(dateEnd)) / (24 * 60 * 60 * 1000) +
+    1
+  );
 }
+// throw new Error('Not implemented');
 
 /**
  * Returns true if a given date is within a specified range, including both the start and end dates.
